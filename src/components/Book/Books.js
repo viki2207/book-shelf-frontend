6302baf9f7c2f8ebec8d7296322de2f022f8c16e
viki2207 <114282438+viki2207@users.noreply.github.com>
2023-04-08 12:12:40 +1000

    Update Books.js

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Book from "./Book";
 import "./Book.css";
-const URL = "http://localhost:5030/books";
+const URL = "https://book-self-bb23.onrender.com/books";
 
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
